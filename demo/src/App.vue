3df4c0a765b8dff3c130a4dbeb2044ed14246eb9
Alan Ktquez <ktquez@gmail.com>
2020-06-19 19:55:31 -0300

    Update demo

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,7 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img
+      alt="Vue logo"
+      src="./assets/logo.png"
+    >
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+
+    <PopupChat :team="team">
+      <template v-slot:header>
+        <h3>Hello!</h3>
+        <p>Click one of our representatives below to chat on WhatsApp or send us an email to <a href="mailto:hello@quadlayers.com">hello@quadlayers.com</a></p>
+      </template>
+      <template v-slot:footer>
+        <a href="mailto:hello@quadlayers.com">hello@quadlayers.com</a>
+      </template>
+    </PopupChat>
   </div>
 </template>
 
@@ -12,7 +25,38 @@ export default {
   name: 'App',
   components: {
     HelloWorld
-  }
+  },
+  data: () => ({
+    team: [
+      {
+        label: 'Support',
+        name: 'Alan Albuquerque',
+        href: 'https://web.whatsapp.com/send?phone=5581983383532&text=',
+        avatar: {
+          src: 'https://avatars0.githubusercontent.com/u/8084606?s=460&u=20b6499a416cf7129a18e5c168cf387e159edb1a&v=4',
+          alt: 'Avatar Support Alan Albuquerque'
+        }
+      },
+      {
+        label: 'Support TI',
+        name: 'Rodrigo Pombo',
+        href: 'https://web.whatsapp.com/send?phone=5581983383532&text=',
+        avatar: {
+          src: 'https://avatars3.githubusercontent.com/u/1911623?s=460&u=e229be53a0816de373a668ad8f74e20b30c056c8&v=4',
+          alt: 'Avatar Support Rodrigo Pombo'
+        }
+      },
+      {
+        label: 'Human Resources',
+        name: 'April Gonçalves',
+        href: 'https://web.whatsapp.com/send?phone=5581983383532&text=',
+        avatar: {
+          src: 'https://avatars3.githubusercontent.com/u/609862?s=400&u=e58f7e1a6a5e5ce21979a183db43870582ffcde4&v=4',
+          alt: 'Avatar Support April Gonçalves'
+        }
+      }
+    ]
+  })
 }
 </script>
 
@@ -24,5 +68,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+a {
+  color: #41b883;
 }
 </style>
--- demo/src/main.js
+++ demo/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueSocialChat from '../vue-social-chat'
 
+Vue.use(VueSocialChat)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(App)
 }).$mount('#app')
