70f74446b4231943edd85cd343dd11586e3998f6
Alan Ktquez <ktquez@gmail.com>
2020-06-19 22:32:48 -0300

    Export individuals component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import VueSocialChat from './plugin'
 
 export default VueSocialChat
+
+export { default as PopupChat } from './PopupChat.vue'
+export { default as WhatsAppChat } from './WhatsAppChat.vue'
